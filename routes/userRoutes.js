000e27de9b938a56f524b9b85ba2ebc81965c4ba
slesha45 <dahalslesha@gmail.com>
2025-01-31 23:48:50 +0545

    routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const userController = require('../controllers/userControllers')
+const { authGuard } = require("../middleware/authGuard");
 
 // Creating user registration route
 router.post('/create', userController.createUser)
@@ -19,6 +20,13 @@ router.post('/verify_otp', userController.verifyOtpAndSetPassword)
 
 router.get("/token", userController.getToken);
 router.get("/profile/get", userController.getCurrentProfile);
+
+
+router.get(("/all"), userController.getAllUsers);
+router.post('/logout',authGuard, userController.logoutUser);
+//get user data
+router.get('/user/:id',userController.getUserDetails);
+router.put("/verifyEmail/:token", userController.verifyEmail);
 
 // Controller(Export)-> Routes (import)-> use ->(index.js)
 
