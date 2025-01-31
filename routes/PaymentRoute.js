36f2b98003e8bac7fa3e0092d24cd8e4e87c86eb
slesha45 <dahalslesha@gmail.com>
2025-01-31 09:32:14 +0545

    creating payment route

---
+++ routes/PaymentRoute.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const {
+  initializeKhalti,
+  completeKhaltiPayment,
+} = require("../controllers/paymentController");
+
+router.post("/initialize_khalti", initializeKhalti);
+router.get("/complete-khalti-payment", completeKhaltiPayment);
+
+module.exports = router;
